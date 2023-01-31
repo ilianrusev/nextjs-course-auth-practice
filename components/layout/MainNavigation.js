3a3b2feeb05ad9d5c3ccab754cf268636ef8b029
Ilian Rusev <ilianrusev@gmail.com>
2023-01-31 22:44:43 +0200

    manage active session on FE

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -1,8 +1,9 @@
 import Link from "next/link";
-
+import { useSession } from "next-auth/client";
 import styles from "./MainNavigation.module.css";
 
 function MainNavigation() {
+  const [session, loading] = useSession();
   return (
     <header className={styles.header}>
       <Link href="/">
@@ -10,15 +11,23 @@ function MainNavigation() {
       </Link>
       <nav>
         <ul>
-          <li>
-            <Link href="/auth">Login</Link>
-          </li>
-          <li>
-            <Link href="/profile">Profile</Link>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
+          {!session && !loading && (
+            <li>
+              <Link href="/auth">Login</Link>
+            </li>
+          )}
+
+          {session && (
+            <li>
+              <Link href="/profile">Profile</Link>
+            </li>
+          )}
+
+          {session && (
+            <li>
+              <button>Logout</button>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
